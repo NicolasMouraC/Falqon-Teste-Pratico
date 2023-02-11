f6aa53bfe7cb71a3a94c0a9dd6d403f8ffd3f3e9
Nicolas <nicolasoutog@gmail.com>
2023-02-11 15:31:42 -0300

    Make header component

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brightPurple': "#7B1FA2",
+        "darkPurple": "#431C60",
+        "lightBlue": "#AAB5E0",
+        "royalBlue": "#4527A0",
+        "vividGreen": "#8BC34A",
+        "Black": "#272727"
+      }
+    },
+  },
+  plugins: [],
+}
